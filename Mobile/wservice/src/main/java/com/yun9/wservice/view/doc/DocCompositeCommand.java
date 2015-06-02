30022b8b1e73fe184e234aa64b949085d21c77e7
ruanxiaoyu <ruanxiaoyu_520@163.com>
2015-06-02 14:16:37 +0800

    1

---
+++ Mobile/wservice/src/main/java/com/yun9/wservice/view/doc/DocCompositeCommand.java
@@ -0,0 +1,19 @@
+package com.yun9.wservice.view.doc;
+
+import com.yun9.jupiter.command.JupiterCommand;
+import com.yun9.jupiter.util.AssertValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Leon on 15/5/30.
+ */
+public class DocCompositeCommand extends JupiterCommand {
+
+    private String instid;
+
+    private boolean edit;
+
+
+}
