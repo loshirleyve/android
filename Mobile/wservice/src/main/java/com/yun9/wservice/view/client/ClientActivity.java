d6e8e6211f07a93af0d92374a0147df949e2e5c8
ll <2634582638@qq.com>
2015-06-11 19:40:15 +0800

    增加了客户管理界面

---
+++ Mobile/wservice/src/main/java/com/yun9/wservice/view/client/ClientActivity.java
@@ -0,0 +1,23 @@
+package com.yun9.wservice.view.client;
+
+import android.os.Bundle;
+
+import com.yun9.jupiter.view.JupiterFragmentActivity;
+import com.yun9.wservice.R;
+
+/**
+ * Created by li on 2015/6/11.
+ */
+public class ClientActivity extends JupiterFragmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_client;
+    }
+
+
+}
---
+++ Mobile/wservice/src/main/java/com/yun9/wservice/view/client/ClientItemLayout.java
@@ -0,0 +1,98 @@
+package com.yun9.wservice.view.client;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yun9.jupiter.widget.JupiterRelativeLayout;
+import com.yun9.wservice.R;
+
+/**
+ * Created by li on 2015/6/11.
+ */
+public class ClientItemLayout extends JupiterRelativeLayout{
+
+    private TextView title_TV;
+    private ImageView contact_IV;
+    private TextView contact_TV;
+    private ImageView phone_IV;
+    private TextView phone_TV;
+    private Button actOrder_BT;
+
+    @Override
+    protected int getContextView() {
+        return R.layout.widget_client_item;
+    }
+
+    @Override
+    protected void initViews(Context context, AttributeSet attrs, int defStyle) {
+        title_TV = (TextView)findViewById(R.id.title_tv);
+        contact_IV = (ImageView)findViewById(R.id.contact_iv);
+        contact_TV = (TextView)findViewById(R.id.contact_tv);
+        phone_IV = (ImageView)findViewById(R.id.phone_iv);
+        phone_TV = (TextView)findViewById(R.id.phone_tv);
+        actOrder_BT = (Button)findViewById(R.id.actOrder_BT);
+    }
+    public TextView getTitle_TV() {
+        return title_TV;
+    }
+
+    public void setTitle_TV(TextView title_TV) {
+        this.title_TV = title_TV;
+    }
+
+    public ImageView getContact_IV() {
+        return contact_IV;
+    }
+
+    public void setContact_IV(ImageView contact_IV) {
+        this.contact_IV = contact_IV;
+    }
+
+    public TextView getContact_TV() {
+        return contact_TV;
+    }
+
+    public void setContact_TV(TextView contact_TV) {
+        this.contact_TV = contact_TV;
+    }
+
+    public ImageView getPhone_IV() {
+        return phone_IV;
+    }
+
+    public void setPhone_IV(ImageView phone_IV) {
+        this.phone_IV = phone_IV;
+    }
+
+    public TextView getPhone_TV() {
+        return phone_TV;
+    }
+
+    public void setPhone_TV(TextView phone_TV) {
+        this.phone_TV = phone_TV;
+    }
+
+    public Button getActOrder_BT() {
+        return actOrder_BT;
+    }
+
+    public void setActOrder_BT(Button actOrder_BT) {
+        this.actOrder_BT = actOrder_BT;
+    }
+
+    public ClientItemLayout(Context context) {
+        super(context);
+    }
+
+    public ClientItemLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ClientItemLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+}
