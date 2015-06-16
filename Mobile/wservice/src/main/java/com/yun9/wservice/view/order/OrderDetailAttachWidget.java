c4595f1c542d90790255c80d6123dc6ad710fb07
CKHuang <ckhuang326@icloud.com>
2015-06-16 11:20:05 +0800

    订单详情，提交资料

---
+++ Mobile/wservice/src/main/java/com/yun9/wservice/view/order/OrderDetailAttachWidget.java
@@ -0,0 +1,50 @@
+package com.yun9.wservice.view.order;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.yun9.jupiter.widget.JupiterRelativeLayout;
+import com.yun9.jupiter.widget.JupiterRowStyleTitleLayout;
+import com.yun9.wservice.R;
+import com.yun9.wservice.model.Order;
+
+/**
+ * Created by huangbinglong on 15/6/16.
+ */
+public class OrderDetailAttachWidget extends JupiterRelativeLayout{
+
+    private JupiterRowStyleTitleLayout titleLayout;
+
+    public OrderDetailAttachWidget(Context context) {
+        super(context);
+    }
+
+    public OrderDetailAttachWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OrderDetailAttachWidget(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void buildViewWithData(Order order) {
+
+    }
+
+    @Override
+    protected int getContextView() {
+        return R.layout.widget_order_detail_attach;
+    }
+
+    @Override
+    protected void initViews(Context context, AttributeSet attrs, int defStyle) {
+        titleLayout = (JupiterRowStyleTitleLayout) this.findViewById(R.id.title_layout);
+        buildView();
+    }
+
+    private void buildView() {
+        titleLayout.getHotNitoceTV().setBackgroundColor(getResources().getColor(R.color.transparent));
+        titleLayout.getHotNitoceTV().setTextColor(getResources().getColor(R.color.black));
+        titleLayout.getHotNitoceTV().setText(R.string.checkout_attach);
+    }
+}
