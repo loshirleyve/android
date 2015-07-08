73a05bec93966b124b3971fb3320890ca9342b01
Leon <63781162@qq.com>
2015-07-08 09:35:54 +0800

    1

--- Mobile/wservice/src/main/java/com/yun9/wservice/MainApplication.java
+++ Mobile/wservice/src/main/java/com/yun9/wservice/MainApplication.java
@@ -3,6 +3,7 @@ package com.yun9.wservice;
 
 import com.yun9.jupiter.app.JupiterApplication;
 import com.yun9.jupiter.manager.SessionManager;
+import com.yun9.wservice.support.OnLoginNoticeBindListener;
 import com.yun9.wservice.support.OnLogoutUnNoticeBindListener;
 
 
@@ -15,6 +16,7 @@ public class MainApplication extends JupiterApplication {
         //注册解除推送绑定动作
         SessionManager sessionManager = this.getBeanManager().get(SessionManager.class);
         sessionManager.regOnLogoutListener(new OnLogoutUnNoticeBindListener());
+        sessionManager.regOnLoginListener(new OnLoginNoticeBindListener());
     }
 
 }
