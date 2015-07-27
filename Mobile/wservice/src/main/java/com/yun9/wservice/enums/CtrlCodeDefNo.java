2a100b2cc54bdc377183620ec3b7cea0420919fe
ll <2634582638@qq.com>
2015-07-27 10:09:36 +0800

    clientDetail

--- Mobile/wservice/src/main/java/com/yun9/wservice/enums/CtrlCodeDefNo.java
+++ Mobile/wservice/src/main/java/com/yun9/wservice/enums/CtrlCodeDefNo.java
@@ -16,4 +16,15 @@ public class CtrlCodeDefNo {
     public static final String WORK_STATE = "workstate";
 
     public static final String ORDER_PAY_STATE = "orderpaystate";
+
+    public static final String CLIENT_TYPE_STATE = "clienttype";
+
+    public static final String CLIENT_INDUSTRY_STATE = "clientindustry";
+
+    public static final String CLIENT_SOURCE_STATE = "clientsource";
+
+    public static final String CLIENT_POSITION_STATE = "contactposition";
+
+    public static final String CLIENT_LEVEL_STATE = "clientlevel";
+
 }
