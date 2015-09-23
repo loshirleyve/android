ca4bba705a7894b03e8319dcd42de9bd9cea2edd
CKHuang <ckhuang326@icloud.com>
2015-09-23 15:31:20 +0800

    完成清理缓存功能

--- Mobile/wservice/src/main/java/com/yun9/wservice/view/other/SettingActivity.java
+++ Mobile/wservice/src/main/java/com/yun9/wservice/view/other/SettingActivity.java
@@ -3,9 +3,15 @@ package com.yun9.wservice.view.other;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.yun9.jupiter.cache.FileCache;
+import com.yun9.jupiter.cache.InstCache;
+import com.yun9.jupiter.cache.UserCache;
 import com.yun9.jupiter.manager.SessionManager;
+import com.yun9.jupiter.util.ImageLoaderUtil;
 import com.yun9.jupiter.view.JupiterFragmentActivity;
 import com.yun9.jupiter.widget.JupiterRowStyleSutitleLayout;
 import com.yun9.jupiter.widget.JupiterTitleBarLayout;
@@ -15,14 +21,16 @@ import com.yun9.wservice.R;
 import com.yun9.wservice.cache.ClientProxyCache;
 import com.yun9.wservice.view.myself.AppAboutActivity;
 
+import java.io.File;
+
 /**
  * Created by Leon on 15/6/9.
  */
 public class SettingActivity extends JupiterFragmentActivity {
 
 
-//    @ViewInject(id = R.id.clean)
-//    private JupiterRowStyleSutitleLayout cleanLayout;
+    @ViewInject(id = R.id.clean)
+    private JupiterRowStyleSutitleLayout cleanLayout;
 
     @ViewInject(id = R.id.about)
     private JupiterRowStyleSutitleLayout aboutLayout;
@@ -58,8 +66,35 @@ public class SettingActivity extends JupiterFragmentActivity {
         });
 
         logoutLayout.setOnClickListener(onLogoutClickListener);
-
         aboutLayout.setOnClickListener(onAboutClickListener);
+        cleanLayout.getTitleTV()
+                .setText(getResources().getString(R.string.setting_clean)
+                        + getTotalDiskCache(ImageLoader.getInstance().getDiskCache().getDirectory()));
+
+        cleanLayout.setOnClickListener(onCleanClickListener);
+    }
+
+    private String getTotalDiskCache(File file) {
+        long kb = 1024;
+        long mb = 1024*1024;
+        String unit = "MB";
+        long totalSize = getTotalSizeOfFilesInDir(file);
+        if (totalSize > mb) {
+            return " ("+totalSize/mb +"MB)";
+        }
+        return " ("+totalSize/kb +"KB)";
+    }
+
+    // 递归方式 计算文件的大小
+    private long getTotalSizeOfFilesInDir(final File file) {
+        if (file.isFile())
+            return file.length();
+        final File[] children = file.listFiles();
+        long total = 0;
+        if (children != null)
+            for (final File child : children)
+                total += getTotalSizeOfFilesInDir(child);
+        return total;
     }
 
     private View.OnClickListener onLogoutClickListener = new View.OnClickListener() {
@@ -75,6 +110,20 @@ public class SettingActivity extends JupiterFragmentActivity {
         @Override
         public void onClick(View v) {
             AppAboutActivity.start(SettingActivity.this);
+        }
+    };
+
+    private View.OnClickListener onCleanClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ImageLoader.getInstance().clearDiskCache();
+            ImageLoader.getInstance().clearMemoryCache();
+            FileCache.getInstance().clean();
+            UserCache.getInstance().clean();
+            InstCache.getInstance().clean();
+            cleanLayout.getTitleTV()
+                    .setText(getResources().getString(R.string.setting_clean)
+                            + getTotalDiskCache(ImageLoader.getInstance().getDiskCache().getDirectory()));
         }
     };
 }
